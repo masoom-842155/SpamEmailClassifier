e58a69657cb3dfa09d486fd9d96b3479ba1d30a6
masoom-842155 <92629879+masoom-842155@users.noreply.github.com>
2025-07-20 12:18:19 +0530

    Update SpamDetection.py

--- SpamDetection.py
+++ SpamDetection.py
@@ -60,7 +60,7 @@ st.markdown("""
 def connect_to_database():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
+            host="https://spamemailclassifier0.streamlit.app/",
             user="root",
             password="masoom842155",
             database="spam_classifier"
